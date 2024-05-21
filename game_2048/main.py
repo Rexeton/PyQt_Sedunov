b63c42d3a63aabc164b45c17d93f07a484766a44
SedynovIvan <SedynovIS@gmail.com>
2024-05-21 17:50:58 +0300

    Доделать: hw_1 Выполнена hw_2 a,b,с,d hw_3 выполнена ДЗ Работа над 2048

--- game_2048/main.py
+++ game_2048/main.py
@@ -8,53 +8,112 @@ class dva_widgets(QtWidgets.QWidget):
         super().__init__(parent)
         self.none_ind = ('00', '01', '02', '03', '10', '11', '12', '13', '20', '21', '22',
                     '23', '30', '31', '32', '33')
+        self.painting_color={'2':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(156, 0, 0);',
+                             '4':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(156, 156, 0);',
+                             '8':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(156, 0, 156);',
+                             '16':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(70, 0, 0);',
+                             '32':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(70, 70, 0);',
+                             '64':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(70, 0, 70);',
+                             '128':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(156, 156, 156);',
+                             '256':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(70, 70, 70);',
+                             '512':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(110, 0, 0);',
+                             '1024':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(0, 110, 0);',
+                             '2048':'font: 700 20pt "Segoe UI";\nbackground-color: rgb(0, 0, 110);'}
         self.full_ind = {}
-        # self.all_id = ['00', '01', '02', '03', '10', '11', '12', '13', '20', '21', '22',
-        #             '23', '30', '31', '32', '33']
-        # self.none_ind = {'00': 1, '01': 1, '02': 1, '03': 1, '10': 1, '11': 1, '12': 1, '13': 1, '20': 1, '21': 1, '22': 1,
-        #             '23': 1, '30': 1, '31': 1, '32': 1, '33': 1}
-        # self.full_ind = {'00': 0, '01': 0, '02': 0, '03': 0, '10': 0, '11': 0, '12': 0, '13': 0, '20': 0, '21': 0, '22': 0,
-        #             '23': 0, '30': 0, '31': 0, '32': 0, '33': 0}
-        self.int_list=[4 * [0],4 * [0],4 * [0],4 * [0]]
+        self.int_list=None
         self.ui=Ui_Game_2048()
         self.ui.setupUi(self)
         self.list_lable=[[self.ui.label_00,self.ui.label_01,self.ui.label_02,self.ui.label_03],[self.ui.label_10,self.ui.label_11,self.ui.label_12,self.ui.label_13],[self.ui.label_20,self.ui.label_21,self.ui.label_22,self.ui.label_23],[self.ui.label_30,self.ui.label_31,self.ui.label_32,self.ui.label_33]]
-        for pow_ in range(3):
-            i,j=self.__new_elem()
-            self.list_lable[i][j].setText(str(2**(pow_+1)))
-            self.int_list[i][j]=2**(pow_+1)
+        self.status=False
+        self._initsign()
+
 
     def __new_elem(self):
         el = rand.choice([el for el in self.none_ind if el not in self.full_ind])
         self.full_ind[el]=1
         return int(el[0]),int(el[1])
 
-    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
-        if len(self.full_ind)==len(self.none_ind):
-            print('game_over')
-        else:
-            if event.key()==QtCore.Qt.Key_Up:
-                self.count_new_V([0,0,0,0],1)
-                self.recount_V([0,1,2],1)
-                self.count_new_V([0, 0, 0, 0], 1)
-            if event.key()==QtCore.Qt.Key_Down:
-                self.count_new_V([3,3,3,3],-1)
-                self.recount_V([3, 2, 1], -1)
-                self.count_new_V([3, 3, 3, 3], -1)
-            if event.key()==QtCore.Qt.Key_Left:
-                self.count_new_H([0,0,0,0],1)
-                self.recount_H([0,1,2],1)
-                self.count_new_H([0, 0, 0, 0], 1)
-            if event.key()==QtCore.Qt.Key_Right:
-                self.count_new_H([3,3,3,3],-1)
-                self.recount_H([3, 2, 1], -1)
-                self.count_new_H([3, 3, 3, 3], -1)
-            self.paint_new()
+    def you_win(self):
+        if max(self.int_list) == 2048:
+            QtWidgets.QMessageBox.warning(self, "Ахренеть", "Ты победил")
+            self.status = False
+            return
+    def game_over(self):
+        if len(self.full_ind) == len(self.none_ind):
+            if self.proverka_ne_prosla():
+                QtWidgets.QMessageBox.warning(self, "game_over", "Больше я тебе ходить не дам")
+                self.status = False
 
+    def repaint_kv(self):
+        if len(self.full_ind) != len(self.none_ind):
             i, j = self.__new_elem()
             self.list_lable[i][j].setText(str(2))
             self.int_list[i][j] = 2
+            self.paint_new()
 
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        self.game_over()
+        if self.status:
+            if event.key()==QtCore.Qt.Key_Up:
+                self.need_up()
+            if event.key()==QtCore.Qt.Key_Down:
+                self.need_down()
+            if event.key()==QtCore.Qt.Key_Left:
+                self.need_left()
+            if event.key()==QtCore.Qt.Key_Right:
+                self.need_right()
+            self.repaint_kv()
+            self.you_win()
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        self.x_pos_was=event.globalPosition().x()
+        self.y_pos_was = event.globalPosition().y()
+
+    def mouseReleaseEvent(self, event: QtGui.QMouseEvent) -> None:
+        self.x_pos_now=event.globalPosition().x()
+        self.y_pos_now =event.globalPosition().y()
+        self.game_over()
+        if self.status and (self.x_pos_was!=self.x_pos_now or self.y_pos_was!=self.y_pos_now):
+            if abs(self.x_pos_was-self.x_pos_now)>abs(self.y_pos_was-self.y_pos_now):
+                if self.x_pos_was-self.x_pos_now>0:
+                    self.need_left()
+                else:
+                    self.need_right()
+            else:
+                if self.y_pos_was - self.y_pos_now > 0:
+                    self.need_up()
+                else:
+                    self.need_down()
+            self.repaint_kv()
+            self.you_win()
+
+    def need_up(self):
+        self.count_new_V([0, 0, 0, 0], 1)
+        self.recount_V([0, 1, 2], 1)
+        self.count_new_V([0, 0, 0, 0], 1)
+    def need_down(self):
+        self.count_new_V([3, 3, 3, 3], -1)
+        self.recount_V([3, 2, 1], -1)
+        self.count_new_V([3, 3, 3, 3], -1)
+    def need_left(self):
+        self.count_new_H([0, 0, 0, 0], 1)
+        self.recount_H([0, 1, 2], 1)
+        self.count_new_H([0, 0, 0, 0], 1)
+    def need_right(self):
+        self.count_new_H([3, 3, 3, 3], -1)
+        self.recount_H([3, 2, 1], -1)
+        self.count_new_H([3, 3, 3, 3], -1)
+    def start_restart_game(self):
+        self.ui.start_pushButton.setText('Начать заново')
+        self.status=True
+        self.full_ind = {}
+        self.int_list=[4 * [0],4 * [0],4 * [0],4 * [0]]
+        for pow_ in range(3):
+            i,j=self.__new_elem()
+            self.list_lable[i][j].setText(str(2**(pow_+1)))
+            self.int_list[i][j]=2**(pow_+1)
+            self.paint_new()
+        self.ui.groupBox.setFocus()
     def count_new_H(self,obl_ind,ind):
         new_ris={}
         self.full_ind=sorted(self.full_ind)[::ind]
@@ -86,9 +145,10 @@ class dva_widgets(QtWidgets.QWidget):
             for j in range(0,4):
                 if self.int_list[i][j]!=0:
                     self.list_lable[i][j].setText(str(self.int_list[i][j]))
+                    self.list_lable[i][j].setStyleSheet(self.painting_color[str(self.int_list[i][j])])
                 else:
                     self.list_lable[i][j].setText('')
-
+                    self.list_lable[i][j].setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
     def recount_H(self,obl_ind, ind):
         for i in range(0,4):
             for j in obl_ind:
@@ -104,6 +164,21 @@ class dva_widgets(QtWidgets.QWidget):
                     self.int_list[i][j]=self.int_list[i][j]*2
                     self.int_list[i+ind][j]=0
                     del self.full_ind[str(i+ind)+str(j)]
+
+    def _initsign(self):
+        self.ui.start_pushButton.clicked.connect(self.start_restart_game)
+        pass
+
+    def proverka_ne_prosla(self):
+            for i in range(0,4):
+                for j in range(1,4):
+                    if self.int_list[i][j]==self.int_list[i][j-1]:
+                        return False
+            for j in range(0,4):
+                for i in range(1,4):
+                    if self.int_list[i][j]==self.int_list[i-1][j]:
+                        return False
+            return True
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
--- game_2048/ui/game_2048.py
+++ game_2048/ui/game_2048.py
@@ -15,41 +15,122 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
-    QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGroupBox, QHBoxLayout,
+    QLabel, QPushButton, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
 
 class Ui_Game_2048(object):
     def setupUi(self, Game_2048):
         if not Game_2048.objectName():
             Game_2048.setObjectName(u"Game_2048")
-        Game_2048.resize(400, 300)
-        self.horizontalLayout = QHBoxLayout(Game_2048)
+        Game_2048.resize(768, 804)
+        self.verticalLayout_5 = QVBoxLayout(Game_2048)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.groupBox_2 = QGroupBox(Game_2048)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setMinimumSize(QSize(0, 70))
+        self.groupBox_2.setMaximumSize(QSize(16777215, 130))
+        self.horizontalLayout_2 = QHBoxLayout(self.groupBox_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.groupBox_4 = QGroupBox(self.groupBox_2)
+        self.groupBox_4.setObjectName(u"groupBox_4")
+        self.groupBox_4.setMinimumSize(QSize(130, 0))
+        self.groupBox_4.setStyleSheet(u"background-color: rgb(255, 255, 190);")
+        self.groupBox_4.setFlat(True)
+        self.verticalLayout_7 = QVBoxLayout(self.groupBox_4)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.label = QLabel(self.groupBox_4)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_7.addWidget(self.label)
+
+        self.label_3 = QLabel(self.groupBox_4)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.label_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_7.addWidget(self.label_3)
+
+
+        self.horizontalLayout_2.addWidget(self.groupBox_4)
+
+        self.groupBox_3 = QGroupBox(self.groupBox_2)
+        self.groupBox_3.setObjectName(u"groupBox_3")
+        self.groupBox_3.setMinimumSize(QSize(130, 0))
+        font = QFont()
+        font.setPointSize(9)
+        self.groupBox_3.setFont(font)
+        self.groupBox_3.setAutoFillBackground(False)
+        self.groupBox_3.setStyleSheet(u"background-color: rgb(255, 255, 190);")
+        self.groupBox_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.groupBox_3.setFlat(True)
+        self.verticalLayout_6 = QVBoxLayout(self.groupBox_3)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.label_2 = QLabel(self.groupBox_3)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_6.addWidget(self.label_2)
+
+        self.label_4 = QLabel(self.groupBox_3)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.label_4.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_6.addWidget(self.label_4)
+
+
+        self.horizontalLayout_2.addWidget(self.groupBox_3)
+
+        self.horizontalSpacer = QSpacerItem(374, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+        self.start_pushButton = QPushButton(self.groupBox_2)
+        self.start_pushButton.setObjectName(u"start_pushButton")
+        self.start_pushButton.setMinimumSize(QSize(0, 100))
+        self.start_pushButton.setStyleSheet(u"background-color: rgb(255, 255, 190);")
+
+        self.horizontalLayout_2.addWidget(self.start_pushButton)
+
+
+        self.verticalLayout_5.addWidget(self.groupBox_2)
+
+        self.groupBox = QGroupBox(Game_2048)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setStyleSheet(u"background-color: rgb(255, 255, 190);")
+        self.horizontalLayout = QHBoxLayout(self.groupBox)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label_00 = QLabel(Game_2048)
+        self.label_00 = QLabel(self.groupBox)
         self.label_00.setObjectName(u"label_00")
+        self.label_00.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_00.setFrameShape(QFrame.Shape.Box)
         self.label_00.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout.addWidget(self.label_00)
 
-        self.label_10 = QLabel(Game_2048)
+        self.label_10 = QLabel(self.groupBox)
         self.label_10.setObjectName(u"label_10")
+        self.label_10.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_10.setFrameShape(QFrame.Shape.Box)
         self.label_10.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout.addWidget(self.label_10)
 
-        self.label_20 = QLabel(Game_2048)
+        self.label_20 = QLabel(self.groupBox)
         self.label_20.setObjectName(u"label_20")
+        self.label_20.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_20.setFrameShape(QFrame.Shape.Box)
         self.label_20.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout.addWidget(self.label_20)
 
-        self.label_30 = QLabel(Game_2048)
+        self.label_30 = QLabel(self.groupBox)
         self.label_30.setObjectName(u"label_30")
+        self.label_30.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_30.setFrameShape(QFrame.Shape.Box)
         self.label_30.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -60,29 +141,33 @@ class Ui_Game_2048(object):
 
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.label_01 = QLabel(Game_2048)
+        self.label_01 = QLabel(self.groupBox)
         self.label_01.setObjectName(u"label_01")
+        self.label_01.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_01.setFrameShape(QFrame.Shape.Box)
         self.label_01.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_2.addWidget(self.label_01)
 
-        self.label_11 = QLabel(Game_2048)
+        self.label_11 = QLabel(self.groupBox)
         self.label_11.setObjectName(u"label_11")
+        self.label_11.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_11.setFrameShape(QFrame.Shape.Box)
         self.label_11.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_2.addWidget(self.label_11)
 
-        self.label_21 = QLabel(Game_2048)
+        self.label_21 = QLabel(self.groupBox)
         self.label_21.setObjectName(u"label_21")
+        self.label_21.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_21.setFrameShape(QFrame.Shape.Box)
         self.label_21.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_2.addWidget(self.label_21)
 
-        self.label_31 = QLabel(Game_2048)
+        self.label_31 = QLabel(self.groupBox)
         self.label_31.setObjectName(u"label_31")
+        self.label_31.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_31.setFrameShape(QFrame.Shape.Box)
         self.label_31.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -93,29 +178,33 @@ class Ui_Game_2048(object):
 
         self.verticalLayout_4 = QVBoxLayout()
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
-        self.label_02 = QLabel(Game_2048)
+        self.label_02 = QLabel(self.groupBox)
         self.label_02.setObjectName(u"label_02")
+        self.label_02.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_02.setFrameShape(QFrame.Shape.Box)
         self.label_02.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_4.addWidget(self.label_02)
 
-        self.label_12 = QLabel(Game_2048)
+        self.label_12 = QLabel(self.groupBox)
         self.label_12.setObjectName(u"label_12")
+        self.label_12.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_12.setFrameShape(QFrame.Shape.Box)
         self.label_12.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_4.addWidget(self.label_12)
 
-        self.label_22 = QLabel(Game_2048)
+        self.label_22 = QLabel(self.groupBox)
         self.label_22.setObjectName(u"label_22")
+        self.label_22.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_22.setFrameShape(QFrame.Shape.Box)
         self.label_22.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_4.addWidget(self.label_22)
 
-        self.label_32 = QLabel(Game_2048)
+        self.label_32 = QLabel(self.groupBox)
         self.label_32.setObjectName(u"label_32")
+        self.label_32.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_32.setFrameShape(QFrame.Shape.Box)
         self.label_32.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -126,29 +215,34 @@ class Ui_Game_2048(object):
 
         self.verticalLayout_3 = QVBoxLayout()
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.label_03 = QLabel(Game_2048)
+        self.label_03 = QLabel(self.groupBox)
         self.label_03.setObjectName(u"label_03")
+        self.label_03.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_03.setFrameShape(QFrame.Shape.Box)
         self.label_03.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label_03.setMargin(1)
 
         self.verticalLayout_3.addWidget(self.label_03)
 
-        self.label_13 = QLabel(Game_2048)
+        self.label_13 = QLabel(self.groupBox)
         self.label_13.setObjectName(u"label_13")
+        self.label_13.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_13.setFrameShape(QFrame.Shape.Box)
         self.label_13.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_3.addWidget(self.label_13)
 
-        self.label_23 = QLabel(Game_2048)
+        self.label_23 = QLabel(self.groupBox)
         self.label_23.setObjectName(u"label_23")
+        self.label_23.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_23.setFrameShape(QFrame.Shape.Box)
         self.label_23.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.verticalLayout_3.addWidget(self.label_23)
 
-        self.label_33 = QLabel(Game_2048)
+        self.label_33 = QLabel(self.groupBox)
         self.label_33.setObjectName(u"label_33")
+        self.label_33.setStyleSheet(u"font: 700 20pt \"Segoe UI\";")
         self.label_33.setFrameShape(QFrame.Shape.Box)
         self.label_33.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -158,6 +252,9 @@ class Ui_Game_2048(object):
         self.horizontalLayout.addLayout(self.verticalLayout_3)
 
 
+        self.verticalLayout_5.addWidget(self.groupBox)
+
+
         self.retranslateUi(Game_2048)
 
         QMetaObject.connectSlotsByName(Game_2048)
@@ -165,6 +262,15 @@ class Ui_Game_2048(object):
 
     def retranslateUi(self, Game_2048):
         Game_2048.setWindowTitle(QCoreApplication.translate("Game_2048", u"2048", None))
+        self.groupBox_2.setTitle("")
+        self.groupBox_4.setTitle("")
+        self.label.setText(QCoreApplication.translate("Game_2048", u"\u0422\u0435\u043a\u0443\u0449\u0438\u0439 \u0440\u0435\u0437\u0443\u043b\u044c\u0442\u0430\u0442", None))
+        self.label_3.setText(QCoreApplication.translate("Game_2048", u"0", None))
+        self.groupBox_3.setTitle("")
+        self.label_2.setText(QCoreApplication.translate("Game_2048", u"\u0420\u0435\u043a\u043e\u0440\u0434", None))
+        self.label_4.setText(QCoreApplication.translate("Game_2048", u"0", None))
+        self.start_pushButton.setText(QCoreApplication.translate("Game_2048", u"\u041d\u0430\u0447\u0430\u0442\u044c \u0438\u0433\u0440\u0443", None))
+        self.groupBox.setTitle("")
         self.label_00.setText("")
         self.label_10.setText("")
         self.label_20.setText("")
